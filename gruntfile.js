58b8653c10b7b1575b739c64e5e34bb947ed2a79
pavan <pavan@pezzo.in>
2016-08-22 09:12:23 +0530

    automating sass to css using grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.initConfig({
         uglify: {
             my_target: {
@@ -9,11 +10,22 @@ module.exports = function (grunt) {
                 }
             }
         }, //uglify
+        compass: {
+            dev: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
         watch: {
             options: {livereload: true},
             scripts: {
                 files: ['components/js/script.js'],
                 tasks: ['uglify']
+            },
+            sass: {
+                files: ['components/scss/*.scss'],
+                tasks: ['compass:dev']
             },
             html: {
                 files: ['*.html']
